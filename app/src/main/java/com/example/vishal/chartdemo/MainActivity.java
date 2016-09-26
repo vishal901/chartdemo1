0ce6f1d37812f97007186c1cb427c414771274eb
vish901 <vishal.patel@dhwaniris.com>
2016-09-26 18:42:04 +0530

    change3

---
+++ app/src/main/java/com/example/vishal/chartdemo/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.vishal.chartdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        BarChart barChart = (BarChart) findViewById(R.id.chart1);
+
+
+        ArrayList<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(4f, 0));
+        entries.add(new BarEntry(8f, 1));
+        entries.add(new BarEntry(6f, 2));
+        entries.add(new BarEntry(12f, 3));
+        entries.add(new BarEntry(18f, 4));
+        entries.add(new BarEntry(9f, 5));
+
+        BarDataSet dataset = new BarDataSet(entries, "# of Calls");
+
+        ArrayList<String> labels = new ArrayList<String>();
+        labels.add("January");
+        labels.add("February");
+        labels.add("March");
+        labels.add("April");
+        labels.add("May");
+        labels.add("June");
+
+        BarData data = new BarData(labels, dataset);
+       // dataset.setColors(ColorTemplate.COLORFUL_COLORS); //
+        barChart.setData(data);
+        barChart.animateY(5000);
+
+
+    }
+
+
+
+
+
+}
